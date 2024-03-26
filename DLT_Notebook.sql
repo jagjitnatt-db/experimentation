bd366ae13fe9a8fb5fd4c49d4e1ad81b863e5c58
jagjitnatt-db <113130959+jagjitnatt-db@users.noreply.github.com>
2024-03-26 08:11:22 -0500

    Final Commit

--- DLT_Notebook.sql
+++ DLT_Notebook.sql
@@ -1,7 +1,8 @@
 -- Databricks notebook source
 CREATE OR REFRESH LIVE TABLE gsk_lt
 AS
-SELECT * FROM jagjitnatt.experimentation.hospitals;
+SELECT * FROM jagjitnatt.experimentation.hospitals
+WHERE 1 = 2;
 
 -- COMMAND ----------
 
